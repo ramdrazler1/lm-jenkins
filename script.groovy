118e4e8f473b22c66640517904627b3e20d1c467
ramdrazler1 <112393496+ramdrazler1@users.noreply.github.com>
2023-07-19 00:03:55 +0530

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,16 @@
+def Docker_Registry_Login(){
+    echo "Logging Into The Nexus Docker Repo"
+    withCredentials([usernamePassword(credentialsId: 'spanartifacts', usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASS')]) {
+    sh "echo \"${NEXUS_PASS}\" | docker login --username \"${NEXUS_USER}\" --password-stdin ${NEXUS_URL}"          
+    }
+}
+
+
+def Push_To_Registry(){
+    echo "Pushing The Docker Images To Nexus Regsitry"
+    sh "docker tag ${Application.toLowerCase()}:${tags} ${NEXUS_URL}/${Application.toLowerCase()}:${tags}"
+    sh "docker push ${NEXUS_URL}/${Application.toLowerCase()}:${tags}" 
+}
+
+
+
